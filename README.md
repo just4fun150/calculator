eb8bf74e79ac40b6c531450e9c2f3419a54e1a51
jus4fun150 <just4fun150@protonmail.com>
2022-07-16 13:13:36 -0400

    completed

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# calculator
+# Calculator
+
+The Odin Project: Calculator
+
+[Live Preview](https://just4fun150.github.io/calculator/) 👀
---
+++ index.js
@@ -0,0 +1,136 @@
+
+
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+};
+
+
+function inputNumber(number)
+{
+    const { displayValue, waitingForSecondOperand } = calculator;
+    if (waitingForSecondOperand === true)
+    {
+        calculator.displayValue = number;
+        calculator.waitingForSecondOperand = false;
+    }
+
+    else
+    {
+        calculator.displayValue = displayValue === '0' ? number : displayValue + number;
+    }
+
+}
+
+function inputDecimal(point)
+{
+    if (calculator.waitingForSecondOperand === true)
+    {
+        calculator.displayValue = '0.';
+        calculator.waitingForSecondOperand = false;
+        return
+    }
+    else if (!calculator.displayValue.includes(point))
+    {
+        calculator.displayValue += point;
+    }
+}
+
+
+function handleOperator(nextOperator)
+{
+    const { firstOperand, displayValue, operator } = calculator
+    const inputValue = parseFloat(displayValue);
+    if (operator && calculator.waitingForSecondOperand)
+    {
+        calculator.operator = nextOperator;
+        return;
+    }
+
+    else if (firstOperand == null && !isNaN(inputValue))
+    {
+        calculator.firstOperand = inputValue;
+    }
+    else if (operator)
+    {
+        const result = operate(firstOperand, inputValue, operator);
+        calculator.displayValue = `${parseFloat(result.toFixed(7))}`;
+        calculator.firstOperand = result;
+    }
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+}
+
+function operate(firstOperand, secondOperand, operator)
+{
+    if (operator === '+')
+    {
+        return firstOperand + secondOperand;
+    } else if (operator === '-')
+    {
+        return firstOperand - secondOperand;
+    } else if (operator === '*')
+    {
+        return firstOperand * secondOperand;
+    } else if (operator === '/')
+    {
+        return firstOperand / secondOperand;
+    }
+    return secondOperand;
+}
+
+function resetCalculator()
+{
+    calculator.displayValue = '0';
+    calculator.firstOperand = null;
+    calculator.waitingForSecondOperand = false;
+    calculator.operator = null;
+    console.log(calculator);
+}
+
+
+function updateDisplay()
+{
+    const display = document.querySelector('.calculator-display');
+    // update the value of the element with the contents of cal obj displayValue
+    display.value = calculator.displayValue;
+}
+updateDisplay();
+
+
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click', event =>
+{
+    const { target } = event;
+    const { value } = target;
+    if (!target.matches('button'))
+    {
+        return;
+    }
+
+    switch (value)
+    {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '=':
+            handleOperator(value);
+            break;
+        case '.':
+            inputDecimal(value);
+            break;
+        case 'all-clear':
+            resetCalculator();
+            break;
+        default:
+            // check if the clicked value is an integer
+            if (Number.isInteger(parseFloat(value)))
+            {
+                inputNumber(value);
+            }
+    }
+    updateDisplay();
+});
